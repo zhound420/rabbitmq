4856a5019ad2546691681517b1b5a9b838cc71be
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-08 10:37:43 -0700

    test3

--- rabbitmq_toolkit.py
+++ rabbitmq_toolkit.py
@@ -1,7 +1,8 @@
 from abc import ABC
 from typing import List
-from superagi.tools.base_tool import BaseToolkit, BaseTool
-from superagi.tools.rabbitmq.rabbitmq_tool import RabbitMQTool
+
+from superagi.tools.base_tool import BaseTool, BaseToolkit
+from rabbitmq_tool import RabbitMQTool
 
 class RabbitMQToolkit(BaseToolkit, ABC):
     name: str = "RabbitMQ Toolkit"
