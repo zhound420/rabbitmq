1572c729ea21b3155b43ac1cb5bea389b235d028
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-09 18:32:59 -0700

    Update rabbitmq_tool.py

--- rabbitmq_tool.py
+++ rabbitmq_tool.py
@@ -1,17 +1,14 @@
 import os
+import json
+import datetime
 import pika
 from abc import ABC
 from typing import Type, Optional, Any
 from pydantic import BaseModel, Field
 from superagi.tools.base_tool import BaseTool
 from rabbitmq_connection import RabbitMQConnection
-import json
-import datetime
 
 class RabbitMQTool(BaseTool, BaseModel):
-    '''
-    Class for RabbitMQ tools. This class includes various tools to interact with RabbitMQ.
-    '''
     logger: Any
     name: str = "RabbitMQTool"
     description: str = "Tool that contains various operations to interact with RabbitMQ"
@@ -20,68 +17,51 @@ class RabbitMQTool(BaseTool, BaseModel):
     rabbitmq_username: str = Field(default_factory=lambda: os.getenv('RABBITMQ_USERNAME', 'guest'))
     rabbitmq_password: str = Field(default_factory=lambda: os.getenv('RABBITMQ_PASSWORD', 'guest'))
 
-    def __init__(self, **data):
-        super().__init__(**data)
-
-    @property
-    def connection_params(self):
-        return pika.ConnectionParameters(host=self.rabbitmq_server, credentials=pika.PlainCredentials(self.rabbitmq_username, self.rabbitmq_password))
+    def build_connection_params(self):
+        credentials = pika.PlainCredentials(self.rabbitmq_username, self.rabbitmq_password)
+        return pika.ConnectionParameters(host=self.rabbitmq_server, credentials=credentials)
 
     def _execute(self, *args, **kwargs):
-        tool_input = kwargs.get('tool_input', {})
+        tool_input = kwargs.get("tool_input", {})
         if isinstance(tool_input, str):
             try:
                 tool_input = json.loads(tool_input)
             except json.JSONDecodeError:
-                tool_input = {
-                    "operation": "send_message",
-                    "receiver": "Linda",  # Placeholder; should be determined dynamically
-                    "content": tool_input
-                }
-
-        operation = tool_input.get('operation')
+                tool_input = {"operation": "send_message", "receiver": "Linda", "message": tool_input}
+        
+        operation = tool_input.get("operation")
         if operation == "send_message":
-            receiver = tool_input.get('receiver')
-            content = tool_input.get('content')
-            priority = tool_input.get('priority', 0)
-            return self.send_message(receiver, content, priority=priority)
+            receiver = tool_input.get("receiver")
+            message = tool_input.get("message")
+            return self.send_message(receiver, message)
         elif operation == "receive_message":
-            queue_name = tool_input.get('queue_name')
+            queue_name = tool_input.get("queue_name")
             return self.receive_message(queue_name)
         else:
-            raise ValueError(f'Unknown operation: \'{operation}\'')
+            raise ValueError(f"Unknown operation: '{operation}'")
 
     def _execute_send(self, receiver, message, persistent=False, priority=0):
-        '''
-        Execute a RabbitMQ send operation.
-        '''
-        connection = RabbitMQConnection(self.connection_params, "send", receiver, message, persistent, priority)
+        connection_params = self.build_connection_params()
+        connection = RabbitMQConnection(connection_params, "send", receiver, message, persistent, priority)
         return connection.run()
 
     def _execute_receive(self, queue_name):
-        '''
-        Execute a RabbitMQ receive operation.
-        '''
-        connection = RabbitMQConnection(self.connection_params, "receive", queue_name)
+        connection_params = self.build_connection_params()
+        connection = RabbitMQConnection(connection_params, "receive", queue_name)
         return connection.run()
 
-    def send_message(self, receiver, content, msg_type="text", priority=0):
-        '''
-        Send a message.
-        '''
+    def send_message(self, receiver, message, msg_type="text", priority=0):
         message = {
             "sender": self.name,
             "receiver": receiver,
             "timestamp": datetime.datetime.now().isoformat(),
             "type": msg_type,
-            "content": content
+            "content": message
         }
         return self._execute_send(receiver, json.dumps(message), priority=priority)
 
     def receive_message(self, queue_name):
-        '''
-        Receive a message.
-        '''
         raw_message = self._execute_receive(queue_name)
         message = json.loads(raw_message)
-        return message['content']
+        return message["content"]
+    
