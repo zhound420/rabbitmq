a887fe4fc061d7fa741800056a53d6e88c67dc94
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-11 05:25:54 -0700

    Update rabbitmq_tool.py

--- rabbitmq_tool.py
+++ rabbitmq_tool.py
@@ -1,47 +1,25 @@
-from superagi.tools.rabbitmq.rabbitmq_connection import RabbitMQConnection
-from superagi.tools.base_tool import BaseToolkit
+from base_toolkit import BaseToolkit
+from rabbitmq_connection import RabbitMQConnection
+
 
 class RabbitMQTool(BaseToolkit):
     def __init__(self, config, operation_type=None, input=None):
-        super().__init__(config, operation_type, input)
-        self.rabbitmq_server = config.rabbitmq_server
-        self.rabbitmq_username = config.rabbitmq_username
-        self.rabbitmq_password = config.rabbitmq_password
-        self.queue_name = config.queue_name
-        self.message = config.message
+        super().__init__(config)
+        self.operation_type = operation_type
+        self.input = input
 
-        if not isinstance(self.queue_name, str):
-            raise TypeError('queue_name must be a string')
+        self.rabbitmq_connection = RabbitMQConnection(config)
 
-    def send_message(self):
-        rabbitmq_connection = RabbitMQConnection(
-            queue_name=self.queue_name,
-            message=self.message,
-            rabbitmq_server=self.rabbitmq_server,
-            rabbitmq_username=self.rabbitmq_username,
-            rabbitmq_password=self.rabbitmq_password,
-        )
-
-        rabbitmq_connection.run()
-
-    def run_operation(self, operation_type, input):
-        if operation_type == "send_message":
-            self.send_message()
+    def process(self):
+        if self.operation_type == "send_message":
+            return self.rabbitmq_connection.send_message(self.input)
+        elif self.operation_type == "receive_message":
+            return self.rabbitmq_connection.receive_message()
         else:
-            raise NotImplementedError
-
-    @staticmethod
-    def get_tool_name():
-        return "rabbitmq"
-
-    @staticmethod
-    def get_operations():
-        return [
-            "send_message",
-        ]
+            raise Exception(f"Unsupported operation_type: {self.operation_type}")
 
     def get_env_keys(self):
-        pass
+        return ["rabbitmq_url"]
 
     def get_tools(self):
-        pass
+        return ["rabbitmq"]
