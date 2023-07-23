2ebcb7c0e5e94510bc6e6fda706a63a03fe6218d
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-22 20:55:49 -0700

    Update rabbitmq_tool.py

--- rabbitmq_tool.py
+++ rabbitmq_tool.py
@@ -20,92 +20,77 @@ class RabbitMQTool(BaseTool, BaseModel):
     rabbitmq_username: str = Field(default_factory=lambda: os.getenv('RABBITMQ_USERNAME', 'guest'))
     rabbitmq_password: str = Field(default_factory=lambda: os.getenv('RABBITMQ_PASSWORD', 'guest'))
 
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.logger = logging.getLogger(__name__)
+        self.agent_name = kwargs.get("agent_name", "RabbitMQTool")
 
     def build_connection_params(self):
         self.logger.debug("Building connection params.")
         credentials = pika.PlainCredentials(self.rabbitmq_username, self.rabbitmq_password)
         self.logger.debug("Connection params built.")
         return pika.ConnectionParameters(host=self.rabbitmq_server, credentials=credentials)
+    
 
-def _execute(self, *args, **kwargs):
-    action_mapping = {
-        "send_message": "send_message",
-        "send": "send_message",
-        "transmit": "send_message",
-        "dispatch": "send_message",
-        "receive_message": "receive_message",
-        "receive": "receive_message",
-        "fetch": "receive_message",
-        "get": "receive_message",
-    }
+    def _execute(self, *args, **kwargs):
+        action_mapping = {
+            "send_message": "send_message",
+            "send": "send_message",
+            "transmit": "send_message",
+            "dispatch": "send_message",
+            "receive_message": "receive_message",
+            "receive": "receive_message",
+            "fetch": "receive_message",
+            "get": "receive_message",
+        }
 
-    tool_input = kwargs.get("tool_input", {})
-    if isinstance(tool_input, str):
-        try:
-            tool_input = json.loads(tool_input)
-        except json.JSONDecodeError:
-            tool_input = {"action": "send_message", "queue_name": self.name, "message": tool_input}
-    else:
-        if "queue_name" not in tool_input or tool_input["queue_name"] is None:
-            tool_input["queue_name"] = self.name
+        tool_input = kwargs.get("tool_input", {})
+        if isinstance(tool_input, str):
+            try:
+                tool_input = json.loads(tool_input)
+            except json.JSONDecodeError:
+                tool_input = {"action": "send_message", "queue_name": self.agent_name, "message": tool_input}
+        else:
+            if "queue_name" not in tool_input or tool_input["queue_name"] is None:
+                tool_input["queue_name"] = self.agent_name
 
-    # If "action" not in tool_input or it's None, use "send_message" as the action
-    tool_input["action"] = tool_input.get("action", "send_message")
+        tool_input["action"] = tool_input.get("action", "send_message")
 
-    action = tool_input.get("action")
-    mapped_action = action_mapping.get(action)
-    if mapped_action == "send_message":
-        queue_name = tool_input.get("queue_name")
-        message = tool_input.get("message")
-        return self._execute_send(queue_name, message)
-    elif mapped_action == "receive_message":
-        queue_name = tool_input.get("queue_name")
-        return self._execute_receive(queue_name)
-    else:
-        raise ValueError(f"Unknown action: '{action}'")
+        action = tool_input.get("action")
+        mapped_action = action_mapping.get(action)
+        if mapped_action == "send_message":
+            queue_name = tool_input.get("queue_name")
+            message = tool_input.get("message")
+            return self._execute_send(queue_name, message)
+        elif mapped_action == "receive_message":
+            queue_name = tool_input.get("queue_name")
+            return self._execute_receive(queue_name)
+        else:
+            raise ValueError(f"Unknown action: '{action}'")
 
-    def _execute_send(self, queue_name, message, persistent=False, priority=0):
-        try:
-            connection_params = self.build_connection_params()
-            connection = RabbitMQConnection(
-                connection_params,
-                operation_type="send",
-                queue_name=queue_name,
-                message=message,
-                persistent=persistent,
-                priority=priority
-            )
-        except (AMQPConnectionError, AMQPChannelError) as e:
-            self.logger.error(f"Error while sending message: {str(e)}")
-        return connection.run()
+    def _execute_send(self, queue_name, message):
+        with RabbitMQConnection(queue_name) as conn:
+            conn.send_message(message)
 
     def _execute_receive(self, queue_name):
-        try:
-            connection_params = self.build_connection_params()
-            connection = RabbitMQConnection(connection_params, "receive", queue_name)
-            return connection.run()
-        except (AMQPConnectionError, AMQPChannelError) as e:
-            self.logger.error(f"Error while receiving message: {str(e)}")
-            return None
+        with RabbitMQConnection(queue_name) as conn:
+            return conn.receive_message()
 
-    def send_message(self, receiver_name, message, msg_type="text", priority=0):
-        queue_name = receiver_name
+    def send_message(self, queue_name, message, msg_type="text", priority=0):
         message = {
-            "sender": self.name,
+            "sender": self.agent_name,
             "receiver": queue_name,
             "timestamp": datetime.datetime.now().isoformat(),
             "type": msg_type,
             "content": message
         }
-        tool_input = {
-            "action": "send_message",
-            "queue_name": queue_name,
-            "message": json.dumps(message)
-        }
-        return self._execute(tool_input=tool_input)
+        return self._execute_send(queue_name, json.dumps(message))
+
+    def receive_message(self, queue_name):
+        raw_message = self._execute_receive(queue_name)
+        message = json.loads(raw_message)
+        return message["content"]
 
     def receive_message(self, receiver_name):
         queue_name = receiver_name
@@ -116,4 +101,4 @@ def _execute(self, *args, **kwargs):
         raw_message = self._execute(tool_input=tool_input)
         message = json.loads(raw_message)
         return message["content"]
-"""
+    
