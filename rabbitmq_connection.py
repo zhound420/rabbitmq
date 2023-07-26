dd7b997697d373f9688dbfb251927bbc4e3455d4
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-25 21:07:55 -0700

    Update rabbitmq_connection.py

--- rabbitmq_connection.py
+++ rabbitmq_connection.py
@@ -14,25 +14,26 @@ class RabbitMQConnection:
         self.logger = logging.getLogger(__name__)
 
     def send(self):
-        with pika.BlockingConnection(self.connection_params) as connection:
-            channel = connection.channel()
-            channel.queue_declare(queue=self.queue_name, durable=True)
-            if self.persistent:
-                properties = pika.BasicProperties(delivery_mode=2, priority=self.priority)
-            else:
-                properties = pika.BasicProperties(priority=self.priority)
-
-            if isinstance(self.queue_name, bytes):
-                self.queue_name = self.queue_name.decode()
-
-            if not isinstance(self.queue_name, str):
-                raise TypeError(f'Expected self.queue_name to be a string, but got {type(self.queue_name).__name__}')
-
-            channel.basic_publish(exchange="", routing_key=self.queue_name, body=self.message, properties=properties)
-            return "Message sent successfully."
+        try:
+            with pika.BlockingConnection(self.connection_params) as connection:
+                channel = connection.channel()
+                channel.queue_declare(queue=self.queue_name, durable=True)
+                if self.persistent:
+                    properties = pika.BasicProperties(delivery_mode=2, priority=self.priority)
+                else:
+                    properties = pika.BasicProperties(priority=self.priority)
+                channel.basic_publish(exchange="", routing_key=self.queue_name, body=self.message, properties=properties)
+                return "Message sent successfully."
+        except (AMQPConnectionError, AMQPChannelError) as e:
+            self.logger.error(f"Error: {str(e)}")
+            return None
 
     def receive(self):
-        with pika.BlockingConnection(self.connection_params) as connection:
-            channel = connection.channel()
-            method_frame, header_frame, body = channel.basic_get(queue=self.queue_name, auto_ack=True)
-            return body
+        try:
+            with pika.BlockingConnection(self.connection_params) as connection:
+                channel = connection.channel()
+                method_frame, header_frame, body = channel.basic_get(queue=self.queue_name, auto_ack=True)
+                return body
+        except (AMQPConnectionError, AMQPChannelError) as e:
+            self.logger.error(f"Error: {str(e)}")
+            return None
