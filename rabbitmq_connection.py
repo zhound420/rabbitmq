e90ee342756d920c806757907f8565111f3a3539
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-26 09:58:33 -0700

    Update rabbitmq_connection.py

--- rabbitmq_connection.py
+++ rabbitmq_connection.py
@@ -1,55 +1,58 @@
 
-import pika
+from pika import PlainCredentials, BlockingConnection, ConnectionParameters
+from pika.exceptions import AMQPConnectionError, AMQPChannelError
 import logging
+from abc import ABC
+from pydantic import BaseModel
+from superagi.tools.base_tool import BaseTool
+import json
 
-class RabbitMQConnection:
-    def __init__(self, connection_params, operation, queue_name, message=None):
+class RabbitMQConnection(BaseTool, BaseModel, ABC):
+    logger: Any
+    connection_params: Any
+    operation: str
+    queue_name: str
+    message: Optional[str] = None
+
+    def __init__(self, connection_params, operation, queue_name, message=None, **kwargs):
+        super().__init__(**kwargs)
+        self.logger = logging.getLogger(__name__)
         self.connection_params = connection_params
         self.operation = operation
         self.queue_name = queue_name
         self.message = message
-        self.logger = logging.getLogger(__name__)
         self.connection = None
         self.channel = None
+        self.connect()
 
-    def __enter__(self):
+    def connect(self):
+        self.logger.debug("Creating a new RabbitMQ connection.")
         try:
-            self.logger.debug("Establishing RabbitMQ connection.")
-            self.connection = pika.BlockingConnection(self.connection_params)
+            self.connection = BlockingConnection(self.connection_params)
             self.channel = self.connection.channel()
             self.logger.debug("RabbitMQ connection established.")
-        except pika.exceptions.AMQPConnectionError as error:
-            self.logger.error(f"Error establishing RabbitMQ connection: {error}")
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.connection is not None:
-            self.logger.debug("Closing RabbitMQ connection.")
-            self.connection.close()
+        except (AMQPConnectionError, AMQPChannelError) as e:
+            self.logger.error("Failed to connect to RabbitMQ.", exc_info=True)
 
     def send(self):
-        if self.channel is not None:
-            self.logger.debug("Sending message.")
-            self.channel.queue_declare(queue=self.queue_name, durable=True)
-            self.channel.basic_publish(
-                exchange='',
-                routing_key=self.queue_name,
-                body=self.message,
-                properties=pika.BasicProperties(delivery_mode=2)  # make message persistent
-            )
-            self.logger.debug("Message sent.")
-        else:
+        self.logger.debug(f"Sending message: {self.message}")
+        if self.channel is None:
             self.logger.error("Cannot send message. Channel is None.")
-        return self.message
+            return
+        self.channel.queue_declare(queue=self.queue_name, durable=True)
+        self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=self.message)
+        self.logger.debug("Message sent.")
 
     def receive(self):
-        if self.channel is not None:
-            self.logger.debug("Receiving message.")
-            self.channel.queue_declare(queue=self.queue_name, durable=True)
-            method_frame, properties, body = self.channel.basic_get(queue=self.queue_name, auto_ack=True)
-            self.logger.debug("Message received.")
-            if method_frame:
-                return body.decode()
-        else:
+        self.logger.debug(f"Receiving message from queue: {self.queue_name}")
+        if self.channel is None:
             self.logger.error("Cannot receive message. Channel is None.")
-        return None
+            return
+        method_frame, header_frame, body = self.channel.basic_get(queue=self.queue_name)
+        if method_frame:
+            self.logger.debug(f"Received message: {body}")
+            self.channel.basic_ack(method_frame.delivery_tag)
+            return body.decode('utf-8')
+        else:
+            self.logger.debug("No message received.")
+            return None
