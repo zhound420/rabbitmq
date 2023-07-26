111b5838de2c5b8bf7e9aa52ce53b3422c92f603
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-26 16:17:51 -0700

    Update rabbitmq_toolkit.py

--- rabbitmq_toolkit.py
+++ rabbitmq_toolkit.py
@@ -1,6 +1,6 @@
 
 import os
-from superagi.common.base_tool import BaseToolkit
+from superagi.tools.base_tool import BaseToolkit
 from superagi.tools.rabbitmq.rabbitmq_tool import RabbitMQTool
 from superagi.tools.rabbitmq.rabbitmq_connection import RabbitMQConnection
 
