a9b7789e5a14e77fe66c61ae48ab835ec0a1836b
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-26 17:39:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from superagi.tools.rabbitmq.rabbitmq_tool import RabbitMQTool
 # You should provide a valid configuration object for RabbitMQTool
 config = {
     'name': 'RabbitMQ-SuperAGI Tool',
-    'rabbitmq_server': 'localhost',
+    'rabbitmq_server': '192.168.4.194',
     'rabbitmq_username': 'guest',
     'rabbitmq_password': 'guest',
     'agent_name': 'my_agent_name',  # Define your agent name directly
