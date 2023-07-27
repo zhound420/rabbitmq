dde30d9b229fcb22c4777ea6f77009485c5d174c
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-26 17:45:43 -0700

    revert 2

--- rabbitmq_tool.py
+++ rabbitmq_tool.py
@@ -9,14 +9,12 @@ from abc import ABC
 from typing import Type, Optional, Any
 from pydantic import BaseModel, Field
 from superagi.tools.base_tool import BaseTool
-from superagi.agent.super_agi import SuperAgi
 from rabbitmq_connection import RabbitMQConnection
 
 class RabbitMQTool(BaseTool, BaseModel):
     logger: Any
-    name: str  
+    name: str  # Added this line
     description: str = "Tool that contains various operations to interact with RabbitMQ"
-    agent_name: str = Field(default_factory=lambda: os.getenv('ai_name', 'superagi'))
 
     rabbitmq_server: str = Field(default_factory=lambda: os.getenv('RABBITMQ_SERVER', '192.168.4.194'))
     rabbitmq_username: str = Field(default_factory=lambda: os.getenv('RABBITMQ_USERNAME', 'guest'))
@@ -31,69 +29,88 @@ class RabbitMQTool(BaseTool, BaseModel):
         credentials = pika.PlainCredentials(self.rabbitmq_username, self.rabbitmq_password)
         self.logger.debug("Connection params built.")
         return pika.ConnectionParameters(host=self.rabbitmq_server, credentials=credentials)
-    
 
-    def _execute(self, *args, agent_name: str = None, **kwargs):
-        action_mapping = {
-            "send_message": self._execute_send,
-            "send": self._execute_send,
-            "transmit": self._execute_send,
-            "dispatch": self._execute_send,
-            "receive_message": self._execute_receive,
-            "receive": self._execute_receive,
-            "fetch": self._execute_receive,
-            "get": self._execute_receive,
+    def _execute(self, *args, **kwargs):
+        operation_mapping = {
+            "send_message": "send_message",
+            "send": "send_message",
+            "transmit": "send_message",
+            "dispatch": "send_message",
+            "receive_message": "receive_message",
+            "receive": "receive_message",
+            "fetch": "receive_message",
+            "get": "receive_message",
         }
-
+        
         tool_input = kwargs.get("tool_input", {})
         if isinstance(tool_input, str):
             try:
                 tool_input = json.loads(tool_input)
             except json.JSONDecodeError:
-                tool_input = {"action": "send_message", "queue_name": self.agent_name, "message": tool_input}
+                tool_input = {"action": "send_message", "queue_name": self.name, "message": tool_input}
         else:
             if "queue_name" not in tool_input or tool_input["queue_name"] is None:
-                tool_input["queue_name"] = agent_name if agent_name is not None else self.agent_name
-
-        tool_input["action"] = tool_input.get("action", "send_message")
+                tool_input["queue_name"] = self.name
 
         action = tool_input.get("action")
-        mapped_action = action_mapping.get(action)
-        if callable(mapped_action):
+        mapped_action = operation_mapping.get(action)
+        if mapped_action == "send_message":
             queue_name = tool_input.get("queue_name")
             message = tool_input.get("message")
-            return mapped_action(queue_name, message)
+            return self._execute_send(queue_name, message)
+        elif mapped_action == "receive_message":
+            queue_name = tool_input.get("queue_name")
+            return self._execute_receive(queue_name)
         else:
-            raise ValueError(f"Unknown action: '{action}'")
+            raise ValueError(f"Unknown operation: '{action}'")
 
-    def _execute_send(self, queue_name, message):
-        with RabbitMQConnection(self.build_connection_params(), 'send', queue_name, message) as conn:
-            if conn is not None:
-                return conn.run()
-            else:
-                self.logger.error("Failed to establish a RabbitMQ connection.")
-            return None
+    def _execute_send(self, queue_name, message, persistent=False, priority=0):
+        try:
+            connection_params = self.build_connection_params()
+            connection = RabbitMQConnection(
+                connection_params,
+                operation_type="send",
+                queue_name=queue_name,
+                message=message,
+                persistent=persistent,
+                priority=priority
+            )
+        except (AMQPConnectionError, AMQPChannelError) as e:
+            self.logger.error(f"Error while sending message: {str(e)}")
+        return connection.run()
 
     def _execute_receive(self, queue_name):
-        connection_params = self.build_connection_params()
-        with RabbitMQConnection(connection_params, 'receive', queue_name) as conn:
-            return conn.run()
+        try:
+            connection_params = self.build_connection_params()
+            connection = RabbitMQConnection(connection_params, "receive", queue_name)
+            return connection.run()
+        except (AMQPConnectionError, AMQPChannelError) as e:
+            self.logger.error(f"Error while receiving message: {str(e)}")
+            return None
 
-    def send_message(self, message, msg_type="text", priority=0, queue_name=None):
-        queue_name = queue_name or self.agent_name
+    def send_message(self, receiver_name, message, msg_type="text", priority=0):
+        queue_name = receiver_name
         message = {
-            "sender": self.agent_name,
+            "sender": self.name,
             "receiver": queue_name,
             "timestamp": datetime.datetime.now().isoformat(),
             "type": msg_type,
             "content": message
         }
-        return self._execute_send(queue_name, json.dumps(message))
+        tool_input = {
+            "action": "send_message",
+            "queue_name": queue_name,
+            "message": json.dumps(message)
+        }
+        return self._execute(tool_input=tool_input)
 
-    def receive_message(self, queue_name=None):
-        queue_name = queue_name or self.agent_name
-        raw_message = self._execute_receive(queue_name)
-        if raw_message:
-            message = json.loads(raw_message)
-            return message["content"]
-        return None
+    def receive_message(self, receiver_name):
+        queue_name = receiver_name
+        tool_input = {
+            "action": "receive_message",
+            "queue_name": queue_name
+        }
+        raw_message = self._execute(tool_input=tool_input)
+        message = json.loads(raw_message)
+        return message["content"]
+"""
--- test.py
+++ test.py
@@ -2,12 +2,9 @@ from superagi.tools.rabbitmq.rabbitmq_tool import RabbitMQTool
 
 # You should provide a valid configuration object for RabbitMQTool
 config = {
-    'name': 'RabbitMQ-SuperAGI Tool',
-    'rabbitmq_server': '192.168.4.194',
+    'rabbitmq_server': 'localhost',
     'rabbitmq_username': 'guest',
     'rabbitmq_password': 'guest',
-    'agent_name': 'my_agent_name',  # Define your agent name directly
 }
-
 tool = RabbitMQTool(**config)
 tool._execute(tool_input={"operation": "send_message", "receiver": "hello", "message": "Hello World!"})
