547a87e8f6f2a2eaec2859557416c621cbcea699
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-28 21:51:03 -0700

    revert2

--- rabbitmq_toolkit.py
+++ rabbitmq_toolkit.py
@@ -1,15 +1,15 @@
-
-from typing import List, Type
-from superagi.tools.base_tool import BaseTool, BaseToolkit
+from superagi.tools.base_tool import BaseToolkit
+from typing import List
 from rabbitmq_send_tool import RabbitMQSendTool
 from rabbitmq_receive_tool import RabbitMQReceiveTool
 
 class RabbitMQToolkit(BaseToolkit):
     name: str = "RabbitMQ Toolkit"
-    description: str = "A toolkit for communicating with a RabbitMQ server"
+    description: str = "A toolkit for sending and receiving messages using RabbitMQ."
 
-    def get_tools(self) -> List[Type[BaseTool]]:
+    def get_tools(self) -> List:
         return [RabbitMQSendTool(), RabbitMQReceiveTool()]
 
     def get_env_keys(self) -> List[str]:
-        return ['RABBITMQ_SERVER', 'RABBITMQ_USERNAME', 'RABBITMQ_PASSWORD']
+        return ["RABBITMQ_SERVER", "RABBITMQ_USERNAME", "RABBITMQ_PASSWORD"]
+    
