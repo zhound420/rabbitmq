d675dcffab003faec025f5cf344af9d4e02a06ce
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-28 20:49:07 -0700

    Update rabbitmq_connection.py

--- rabbitmq_connection.py
+++ rabbitmq_connection.py
@@ -16,10 +16,14 @@ class RabbitMQConnection:
         self.channel = self.connection.channel()
 
     def send_message(self, queue_name, message, persistent, priority):
+        # Declare the queue before sending a message
+        self.channel.queue_declare(queue=queue_name, durable=True)
         properties = pika.BasicProperties(delivery_mode=persistent, priority=priority)
         self.channel.basic_publish(exchange='', routing_key=queue_name, body=message, properties=properties)
 
     def receive_message(self, queue_name):
+        # Declare the queue before receiving a message
+        self.channel.queue_declare(queue=queue_name, durable=True)
         method_frame, header_frame, body = self.channel.basic_get(queue_name)
         if method_frame:
             self.channel.basic_ack(method_frame.delivery_tag)
