0a35bd701d1e74bf651cde4f3989525f6889b71c
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-28 22:59:43 -0700

    Update rabbitmq_connection.py

--- rabbitmq_connection.py
+++ rabbitmq_connection.py
@@ -1,30 +1,19 @@
 import pika
+from abc import ABC, abstractmethod
 
-class RabbitMQConnection:
-    def __init__(self, server, username, password):
-        self.credentials = pika.PlainCredentials(username, password)
-        self.parameters = pika.ConnectionParameters(server, credentials=self.credentials)
-        self.connection = None
-        self.channel = None
-
-    def open_connection(self):
-        self.connection = pika.BlockingConnection(self.parameters)
+class RabbitMQConnection(ABC):
+    def __init__(self, server: str, username: str, password: str):
+        credentials = pika.PlainCredentials(username, password)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(server, credentials=credentials))
         self.channel = self.connection.channel()
+        
+    @abstractmethod
+    def send_message(self, queue_name: str, message: str, persistent: int, priority: int):
+        pass
+    
+    @abstractmethod
+    def receive_message(self, queue_name: str):
+        pass
 
     def close_connection(self):
-        if self.connection:
-            self.connection.close()
-
-    def send_message(self, queue_name, message, persistent, priority):
-        self.channel.queue_declare(queue=queue_name, durable=True)
-        properties = pika.BasicProperties(delivery_mode=persistent, priority=priority)
-        self.channel.basic_publish(exchange='', routing_key=queue_name, body=message, properties=properties)
-
-    def receive_message(self, queue_name):
-        self.channel.queue_declare(queue=queue_name, durable=True)
-        method_frame, header_frame, body = self.channel.basic_get(queue_name)
-        if method_frame:
-            self.channel.basic_ack(method_frame.delivery_tag)
-            return body.decode('utf-8')
-        else:
-            return None
+        self.connection.close()
