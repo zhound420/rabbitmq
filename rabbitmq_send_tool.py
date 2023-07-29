c1412dbd9f6c5d34322c950db8f473b6e2de33df
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-28 20:21:11 -0700

    v5

--- rabbitmq_send_tool.py
+++ rabbitmq_send_tool.py
@@ -1,6 +1,6 @@
-from typing import Type
 from superagi.tools.base_tool import BaseTool
 from pydantic import BaseModel, Field
+from typing import Type
 from rabbitmq_connection import RabbitMQConnection
 
 class RabbitMQSendToolInput(BaseModel):
@@ -13,9 +13,18 @@ class RabbitMQSendTool(BaseTool):
     name: str = "RabbitMQ Send Tool"
     args_schema: Type[BaseModel] = RabbitMQSendToolInput
     description: str = "A tool for sending messages to a RabbitMQ server."
-    rabbitmq_connection: RabbitMQConnection = RabbitMQConnection()
+    rabbitmq_connection: RabbitMQConnection = None
 
     def _execute(self, queue_name: str, message: str, persistent: int, priority: int):
+        # Initialize the RabbitMQ connection if it's not already initialized
+        if self.rabbitmq_connection is None:
+            self.rabbitmq_connection = RabbitMQConnection(
+                server=self.get_tool_config('RABBITMQ_SERVER'),
+                username=self.get_tool_config('RABBITMQ_USERNAME'),
+                password=self.get_tool_config('RABBITMQ_PASSWORD')
+            )
+
         # Use the RabbitMQConnection to establish a connection and send a message
         self.rabbitmq_connection.connect()
         self.rabbitmq_connection.send_message(queue_name, message, persistent, priority)
+        
