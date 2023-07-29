29cda6c40db6fb79dfbb7a006051aa5e53d0904d
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-28 21:09:46 -0700

    v6

--- rabbitmq_send_tool.py
+++ rabbitmq_send_tool.py
@@ -22,9 +22,28 @@ class RabbitMQSendTool(BaseTool):
                 server=self.get_tool_config('RABBITMQ_SERVER'),
                 username=self.get_tool_config('RABBITMQ_USERNAME'),
                 password=self.get_tool_config('RABBITMQ_PASSWORD')
+from pydantic import BaseModel, Field
+from typing import Type
+from superagi.tools.base_tool import BaseTool
+from rabbitmq_connection import RabbitMQConnection
+
+class RabbitMQSendToolInput(BaseModel):
+    message: str = Field(..., description="Message to be sent")
+    persistent: int = Field(1, description="Should the message be persistent? 1 for yes, 0 for no")
+    priority: int = Field(0, description="Priority of the message")
+
+class RabbitMQSendTool(BaseTool):
+    name: str = "RabbitMQ Send Tool"
+    args_schema: Type[BaseModel] = RabbitMQSendToolInput
+    description: str = "Tool for sending a message to a RabbitMQ queue"
+    queue_name: str  # Define the queue name
+
+    rabbitmq_connection: RabbitMQConnection = RabbitMQConnection()
+
+    def _execute(self, message: str, persistent: int, priority: int):
+        self.rabbitmq_connection.send_message(self.queue_name, message, persistent, priority)
             )
 
         # Use the RabbitMQConnection to establish a connection and send a message
         self.rabbitmq_connection.connect()
         self.rabbitmq_connection.send_message(queue_name, message, persistent, priority)
-        
