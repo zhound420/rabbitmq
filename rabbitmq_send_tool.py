7aef403d122c1ca645902bd5cee8c6692c60cb83
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-28 23:16:08 -0700

    Update rabbitmq_send_tool.py

--- rabbitmq_send_tool.py
+++ rabbitmq_send_tool.py
@@ -8,14 +8,21 @@ class RabbitMQSendToolInput(BaseModel):
     persistent: int = Field(1, description="Should the message be persistent? 1 for yes, 0 for no")
     priority: int = Field(0, description="Priority of the message")
 
-class RabbitMQSendTool(RabbitMQConnection, BaseTool):
+class RabbitMQSendTool(BaseTool):
     name: str = "RabbitMQ Send Tool"
     args_schema: Type[BaseModel] = RabbitMQSendToolInput
     description: str = "Tool for sending a message to a RabbitMQ queue"
     queue_name: str = "conversation_queue"  # Define the queue name
 
     def _execute(self, message: str, persistent: int, priority: int):
-        self.send_message(self.queue_name, message, persistent, priority)
+        # Initialize a new RabbitMQConnection
+        rabbitmq_connection = RabbitMQConnection(
+            server=self.get_tool_config('RABBITMQ_SERVER'),
+            username=self.get_tool_config('RABBITMQ_USERNAME'),
+            password=self.get_tool_config('RABBITMQ_PASSWORD')
+        )
+        # Use the RabbitMQConnection to send a message
+        rabbitmq_connection.send_message(self.queue_name, message, persistent, priority)
         
     def send_message(self, queue_name: str, message: str, persistent: int, priority: int):
         self.channel.queue_declare(queue=queue_name, durable=True)
