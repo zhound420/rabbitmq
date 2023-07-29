6b488ab5bb880c15576ce13c80c8cfa4b88d718d
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-28 17:54:10 -0700

    Update rabbitmq_tool.py

--- rabbitmq_tool.py
+++ rabbitmq_tool.py
@@ -1,75 +1,53 @@
-from typing import Any
-from superagi.tools.base_tool import BaseTool
+
+from typing import Optional
+from pydantic import BaseModel
+from superagr.superclasses.superagent import SuperAgent, SuperAgentConfig
+from superagr.superclasses.superagent import AgentConnection as SuperAgentConnection
+from superagr.superclasses.superagent import AgentMessage as SuperAgentMessage
 import pika
-import os
-import logging
-import datetime
-import json
-from rabbitmq_connection import RabbitMQConnection
-from pydantic import BaseModel, Field
-from pydantic import BaseSettings
+from pika import PlainCredentials
+from pika.adapters.blocking_connection import BlockingChannel
 
 
-class RabbitMQToolConfig(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
+class RabbitMQConnection(SuperAgentConnection):
+    connection: Optional[pika.BlockingConnection] = None
+    channel: Optional[BlockingChannel] = None
 
-    name: str = "RabbitMQ Tool"
-    description: str = "A tool for interacting with RabbitMQ"
-    rabbitmq_server: str = os.getenv('RABBITMQ_SERVER', 'localhost')
-    rabbitmq_username: str = os.getenv('RABBITMQ_USERNAME', 'guest')
-    rabbitmq_password: str = os.getenv('RABBITMQ_PASSWORD', 'guest')
-    rabbitmq_virtual_host: str = os.getenv('RABBITMQ_VIRTUAL_HOST', '/')
-    rabbitmq_port: int = os.getenv('RABBITMQ_PORT', 5672)
-    connection_params: Any = pika.ConnectionParameters(
-        host=rabbitmq_server,
-        port=rabbitmq_port,
-        virtual_host=rabbitmq_virtual_host,
-        credentials=pika.PlainCredentials(rabbitmq_username, rabbitmq_password)
-    )
+    def __init__(self, connection_params):
+        credentials = PlainCredentials(connection_params.username, connection_params.password)
+        parameters = pika.ConnectionParameters(connection_params.host,
+                                               connection_params.port,
+                                               '/',
+                                               credentials)
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+
+    def __del__(self):
+        if self.connection:
+            self.connection.close()
 
 
-class RabbitMQTool(BaseTool):
-    config: RabbitMQToolConfig
+class RabbitMQMessage(SuperAgentMessage):
+    pass
+
+
+class RabbitMQConfig(SuperAgentConfig):
+    host: str
+    port: str
+    username: str
+    password: str
+
+
+class RabbitMQAgent(SuperAgent):
+    config: RabbitMQConfig
     connection: RabbitMQConnection
+    message: Optional[RabbitMQMessage] = None
 
-    def __init__(self, name: str, description: str, config: RabbitMQToolConfig):
+    def __init__(self, config: RabbitMQConfig, name: str, description: str):
         super().__init__(name=name, description=description)
         self.config = config
-        self.connection = RabbitMQConnection(self.config.connection_params)
-        super().__init__()
-        self.name = name
-        self.description = description
-        self.config = config
-        self.connection = RabbitMQConnection(self.config.connection_params)
-        super().__init__()
+        self.connection = RabbitMQConnection(self.config)
 
     def __del__(self):
         if hasattr(self, 'connection'):
-            self.connection.close()
-
-    def _execute(self, action, queue_name, message=None, msg_type="text", priority=0):
-        # Enhanced error handling
-        try:
-            if action == "send":
-                self.send_natural_language_message(queue_name, message, msg_type, priority)
-            elif action == "receive":
-                return self.receive_natural_language_message(queue_name)
-            else:
-                raise ValueError(f"Unsupported action: {action}")
-        except Exception as e:
-            # Log the error and re-raise the exception
-            self.logger.error(f"An error occurred during execution: {e}")
-            raise
-
-    ...
-
-    def send_natural_language_message(self, receiver, content, msg_type="text", priority=0):
-        message = {
-            "sender": self.config.name,
-            "receiver": receiver,
-            "timestamp": datetime.datetime.now().isoformat(),
-            "type": msg_type,
-            "content": content
-        }
-        self.execute("send", receiver, json.dumps(message), priority=priority)
+            del self.connection
