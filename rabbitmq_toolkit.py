9103e8858acb8cb1015a51cb3c34057f7f315ee6
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-01 21:31:03 -0700

    v4

--- rabbitmq_toolkit.py
+++ rabbitmq_toolkit.py
@@ -2,6 +2,8 @@ from superagi.tools.base_tool import BaseToolkit
 from typing import List
 from rabbitmq_send_tool import RabbitMQSendTool
 from rabbitmq_receive_tool import RabbitMQReceiveTool
+from superagi.agent.super_agi import SuperAGI
+
 
 class RabbitMQToolkit(BaseToolkit):
     name: str = "RabbitMQ Toolkit"
