15df82dd25d1b411193e05df3e7f426a8d660b7c
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-01 20:35:54 -0700

    v7

--- rabbitmq_send_tool.py
+++ rabbitmq_send_tool.py
@@ -1,33 +1,33 @@
-from superagi.tools.base_tool import BaseTool
+
+import pika
+import json
+from typing import Any
 from pydantic import BaseModel, Field
-from typing import Type
-from rabbitmq_connection import RabbitMQConnection
-from superagi.agent.super_agi import SuperAgi
+
+from superagi.tools.base_tool import BaseTool
 
 class RabbitMQSendToolInput(BaseModel):
-    ai_name: str
-    queue_name: str = Field(..., description="Name of the RabbitMQ queue to send message to")
-    message: str = Field(..., description="Message to be sent")
-    persistent: int = Field(..., description="Message persistence. 1 for non-persistent, 2 for persistent")
-    priority: int = Field(..., description="Message priority")
-
+    message: Any = Field(..., description="Message to be sent")
+    queue_name: str = Field(..., description="Name of the RabbitMQ queue to send the message to")
 
 class RabbitMQSendTool(BaseTool):
     name: str = "RabbitMQ Send Tool"
     args_schema: Type[BaseModel] = RabbitMQSendToolInput
-    description: str = "This tool sends a message to a RabbitMQ queue."
+    description: str = "This tool sends a message to a specified RabbitMQ queue"
 
-    def _execute(self, ai_name: str, message: str, persistent: int = 1, priority: int = 1):
-        queue_name = f"{ai_name}_queue"
+    def __init__(self, ai_name):
+        super().__init__()
+        self.ai_name = ai_name
 
-        if self.rabbitmq_connection is None:
-            self.rabbitmq_connection = RabbitMQConnection(
-                server=self.get_tool_config('RABBITMQ_SERVER'),
-                username=self.get_tool_config('RABBITMQ_USERNAME'),
-                password=self.get_tool_config('RABBITMQ_PASSWORD')
-            )
+    def _execute(self, message: str = None, queue_name: str = None):
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
 
-        # Use the RabbitMQConnection to establish a connection and send a message
-        self.rabbitmq_connection.connect()
-        return self.rabbitmq_connection.send_message(f"{ai_name}_queue", message, persistent, priority) or "Message sent" or "Message sent" or "Message sent"
-        
+        queue_name = self.ai_name + "_" + queue_name
+        channel.queue_declare(queue=queue_name)
+
+        channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(message))
+
+        connection.close()
+
+        return "Sent"
